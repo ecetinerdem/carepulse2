f7481f1d9b3772d698f1d4ed5d1fbbaaaf7dd039
Cetin Erdem <cetoerdem1@gmail.com>
2024-12-13 11:51:01 +0400

    add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
-  return <h1 className="text-3xl underline">Home</h1>;
+  return (
+    <div>
+      <h1 className="text-white text-3xl underline">Home</h1>
+      <Button>Button</Button>
+    </div>
+  );
 }
