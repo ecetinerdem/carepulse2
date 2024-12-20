71516447e5b6450e082610e7220b6802275efecf
Cetin Erdem <cetoerdem1@gmail.com>
2024-12-20 19:00:45 +0400

    add detailed submitbutton

--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { Button } from "./ui/button";
+import Image from "next/image";
 
-const SubmitButton = () => {
-  return <div>SubmitButton</div>;
+interface ButtonProps {
+  isLoading: boolean;
+  clasName: string;
+  children: React.ReactNode;
+}
+
+const SubmitButton = ({ isLoading, clasName, children }: ButtonProps) => {
+  return (
+    <Button
+      type="submit"
+      disabled={isLoading}
+      className={clasName ?? "shad-primary-btn w-full"}
+    >
+      {isLoading ? (
+        <div className="flex items-center gap-4">
+          <Image
+            src="/assests/icons/loader.svg"
+            alt="loader"
+            width={24}
+            height={24}
+            className="animate-spin"
+          />
+          Loading...
+        </div>
+      ) : (
+        children
+      )}
+    </Button>
+  );
 };
 
 export default SubmitButton;
