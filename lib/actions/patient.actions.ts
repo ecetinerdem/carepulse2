2e5f0d60518840b0e572d371ae22ee8660d05f61
Cetin Erdem <cetoerdem1@gmail.com>
2024-12-23 00:04:31 +0400

    fix validations

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,20 +1,29 @@
 import { ID, Query } from "node-appwrite"
+import { users } from "../appwrite.config"
+import { parseStringify } from "../utils";
 
+// CREATE APPWRITE USER
 export const createUser = async (user: CreateUserParams) => {
     try {
-        const newUser = await users.create(
+      // Create new user -> https://appwrite.io/docs/references/1.5.x/server-nodejs/users#create
+      const newuser = await users.create(
         ID.unique(),
         user.email,
         user.phone,
         undefined,
         user.name
-    )
+      );
+  
+      return parseStringify(newuser);
     } catch (error: any) {
-        if(error && error?.code === 409) {
-            const documents = await users.list([
-                Query.equal('email', [user.email])
-            ])
-            return documents?.users[0]
-        }
+      // Check existing user
+      if (error && error?.code === 409) {
+        const existingUser = await users.list([
+          Query.equal("email", [user.email]),
+        ]);
+  
+        return existingUser.users[0];
+      }
+      console.error("An error occurred while creating a new user:", error);
     }
-}
+  };
