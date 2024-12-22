bb44a318b94d7cc5196c4647eb4bf0abf29b79a3
Cetin Erdem <cetoerdem1@gmail.com>
2024-12-22 11:47:05 +0400

    add create user

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -10,6 +10,7 @@ import SubmitButton from "../SubmitButton";
 import { useState } from "react";
 import { UserFormValidation } from "@/lib/validation";
 import { useRouter } from "next/navigation";
+import { createUser } from "@/lib/actions/patient.actions";
 
 export enum FormFieldType {
   INPUT = "input",
@@ -44,13 +45,13 @@ const PatientForm = () => {
     // ✅ This will be type-safe and validated.
     setIsLoading(true);
     try {
-      //const userData = {
-      //name,
-      //email,
-      //phone,
-      //};
-      //const user = await createUser(userData);
-      //if (user) router.push(`/patients/${user.$id}/register`);
+      const userData = {
+        name,
+        email,
+        phone,
+      };
+      const user = await createUser(userData);
+      if (user) router.push(`/patients/${user.$id}/register`);
     } catch (error) {
       console.log(error);
     }
